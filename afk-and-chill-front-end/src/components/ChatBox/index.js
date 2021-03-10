04f943dbea8fb109a86fe538a45572bb81ea04e7
karenm08 <wmok14@my.bcit.ca>
2021-03-10 15:12:06 -0800

    fixing tabbing

--- afk-and-chill-front-end/src/components/ChatBox/index.js
+++ afk-and-chill-front-end/src/components/ChatBox/index.js
@@ -1,78 +1,92 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import { Avatar, CardHeader, Typography, Card, CardActions, CardContent, CardMedia } from '@material-ui/core'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+    Avatar,
+    CardHeader,
+    Typography,
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+} from '@material-ui/core';
 
-import MessageForm from '../MessageForm'
-import UserMessage from '../UserMessage'
+import MessageForm from '../MessageForm';
+import UserMessage from '../UserMessage';
 
 const useStyles = makeStyles({
-  root: {
-    maxWidth: 1200,
-    height: 600,
-    display: "flex",
-  },
-  media: {
-    width: "50%"
-  },
-  
-  otherData: {
-    width: "40%",
-    display: "flex",
-    flexDirection: "column",
-    boxSizing: "border-box",
-    justifyContent: "space-between",
-    margin: "0 10px",
-  },
-  messageForm: {
-    width: "100%"
-  },
-  message: {
-    margin: "10px 0"
-  },
-  messages: {
-    overflowY: "scroll"
-  }
-})
+    root: {
+        maxWidth: 1200,
+        height: 600,
+        display: 'flex',
+    },
+    media: {
+        width: '50%',
+    },
 
-export default function ChatBox({className, message, submitMessage}) {
-  const classes = useStyles()
+    otherData: {
+        width: '40%',
+        display: 'flex',
+        flexDirection: 'column',
+        boxSizing: 'border-box',
+        justifyContent: 'space-between',
+        margin: '0 10px',
+    },
+    messageForm: {
+        width: '100%',
+    },
+    message: {
+        margin: '10px 0',
+    },
+    messages: {
+        overflowY: 'scroll',
+    },
+});
 
-  const onMessage = data => {
-    submitMessage({chatId: message._id, text: data.message})
-  }
+export default function ChatBox({ className, message, submitMessage }) {
+    const classes = useStyles();
 
-  return (
-    <Card className={`${classes.root} ${className}`}>
-        <Card className={classes.media}>
-            <CardHeader 
-            avatar={
-            <Avatar className={classes.avatar}>
-                {message.chiller[0]}
-            </Avatar>}
-            title={message.chiller}
-            />
-        </Card>
-        <div className={classes.otherData}>
-            <CardHeader
-            avatar={
-            <Avatar className={classes.avatar}>
-                {message.username[0]}
-            </Avatar>}
-            title={message.username}
-            />
-            <CardContent className={classes.message}>
-            {message.messages.map(message => (
-            <UserMessage key={message.id} className={classes.message} message={message}></UserMessage>
-            ))}
+    const onMessage = (data) => {
+        submitMessage({ chatId: message._id, text: data.message });
+    };
 
-            </CardContent>
-            <div>
-                <CardActions>
-                </CardActions>
-                <MessageForm className={classes.messageForm} onSubmit={onMessage}></MessageForm>
+    return (
+        <Card className={`${classes.root} ${className}`}>
+            <Card className={classes.media}>
+                <CardHeader
+                    avatar={
+                        <Avatar className={classes.avatar}>
+                            {message.chiller[0]}
+                        </Avatar>
+                    }
+                    title={message.chiller}
+                />
+            </Card>
+            <div className={classes.otherData}>
+                <CardHeader
+                    avatar={
+                        <Avatar className={classes.avatar}>
+                            {message.username[0]}
+                        </Avatar>
+                    }
+                    title={message.username}
+                />
+                <CardContent className={classes.message}>
+                    {message.messages.map((message) => (
+                        <UserMessage
+                            key={message._id}
+                            className={classes.message}
+                            message={message}
+                        ></UserMessage>
+                    ))}
+                </CardContent>
+                <div>
+                    <CardActions></CardActions>
+                    <MessageForm
+                        className={classes.messageForm}
+                        onSubmit={onMessage}
+                    ></MessageForm>
+                </div>
             </div>
-        </div>
-      
-    </Card>
-  )
+        </Card>
+    );
 }
