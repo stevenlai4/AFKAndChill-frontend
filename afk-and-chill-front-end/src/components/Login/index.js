6e71e7470b1fed4a3cf5a6e8940d69fc71f02681
Kalvin Tang <ktang82@my.bcit.ca>
2021-03-12 06:22:12 -0800

    deleted carousel

--- afk-and-chill-front-end/src/components/Login/index.js
+++ afk-and-chill-front-end/src/components/Login/index.js
@@ -1,24 +1,47 @@
-import React, { useState } from 'react';
-import Card from '@material-ui/core/Card';
-import { makeStyles } from '@material-ui/core/styles';
-import { Auth } from 'aws-amplify';
-import { Link } from 'react-router-dom';
-import { useHistory } from 'react-router-dom';
-import { TextField, Button } from '@material-ui/core';
+import React, { useState } from "react";
+import Card from "@material-ui/core/Card";
+import { makeStyles } from "@material-ui/core/styles";
+import { Auth } from "aws-amplify";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { TextField, Button } from "@material-ui/core";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Carousel from "react-bootstrap/Carousel";
+import { Avatar } from "@material-ui/core";
 
 const useStyles = makeStyles({
     root: {
-        maxWidth: 275,
+        flexGrow: 1,
+        position: "fixed",
+        // top: 0,
+        width: "25%",
+        // zIndex: 100,
+    },
+    formControl: {
+        minWidth: 100,
+    },
+    login: {
+        display: "flex",
+        justifyContent: "space-between",
     },
     title: {
         fontSize: 14,
+    },
+
+    input: {
+        marginBottom: 20,
+        width: "200%",
+    },
+
+    wrapper: {
+        margin: 30,
     },
 });
 
 function Login({ setisAuthenticated }) {
     const classes = useStyles();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const history = useHistory();
 
     const onSubmit = async (e) => {
@@ -30,8 +53,8 @@ function Login({ setisAuthenticated }) {
                 password,
             });
             setisAuthenticated(true);
-            history.push('/chatBox');
-            console.log('Login Successful');
+            history.push("/chatBox");
+            console.log("Login Successful");
             console.log(user);
         } catch (error) {
             console.error(error.message);
@@ -40,12 +63,18 @@ function Login({ setisAuthenticated }) {
 
     return (
         <section
+            className={classes.wrapper}
             className="Login-form"
-            style={{ flex: 'row', flexDirection: 'column' }}
+            style={{ display: "flex", flexDirection: "row" }}
         >
             <h1>AFK & Chill </h1>
+            <div>
+                <img src="https://i.imgur.com/x7EZzDz.jpg" alt="AFK home" />
+            </div>
+
             <Card className={classes.root}>
                 <h3>Welcome to AFK & Chill</h3>
+                <Avatar src="/broken-image.jpg" />
                 <form onSubmit={onSubmit}>
                     <div className="control">
                         <TextField
@@ -77,7 +106,7 @@ function Login({ setisAuthenticated }) {
                     </div>
 
                     <div>
-                        Dont have an account?{' '}
+                        Dont have an account?{" "}
                         <Link to="/register">Create an Account</Link>
                     </div>
                 </form>
