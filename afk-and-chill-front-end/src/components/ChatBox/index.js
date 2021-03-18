e3d1ec72fcefa2d35cd4f6739ea797d19b436214
karenm08 <wmok14@my.bcit.ca>
2021-03-18 14:02:09 -0700

    styling chatpage

--- afk-and-chill-front-end/src/components/ChatBox/index.js
+++ afk-and-chill-front-end/src/components/ChatBox/index.js
@@ -4,7 +4,6 @@ import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
 import { Avatar, CardHeader, Card, CardContent } from '@material-ui/core';
@@ -28,7 +27,7 @@ const useStyles = makeStyles((theme) => ({
         },
     },
     chatBox: {
-        width: '50%',
+        width: '60%',
         height: 650,
         display: 'flex',
         margin: 10,
@@ -38,17 +37,25 @@ const useStyles = makeStyles((theme) => ({
         },
     },
     chillerItem: {
-        width: '50%',
+        width: '40%',
         margin: 10,
         [theme.breakpoints.down('sm')]: {
             display: 'none',
         },
     },
     chillerItemCard: {
-        // marginTop: '5%',
         display: 'flex',
         alignItems: 'center',
         cursor: 'pointer',
+    },
+    chillerItemCardDrawer: {
+        display: 'flex',
+        alignItems: 'center',
+        cursor: 'pointer',
+    },
+    textDrawer: { marginLeft: '10px' },
+    avatarDrawer: {
+        margin: '5%',
     },
     avatar: {
         margin: '3%',
@@ -117,57 +124,34 @@ export default function ChatBox({
             onClick={toggleDrawer(anchor, false)}
             onKeyDown={toggleDrawer(anchor, false)}
         >
-            {/* {chatboxes.map((chatItem) => (
-                <Card
-                    className={classes.chillerCard}
-                    onClick={() => onChatItem(chatItem._id)}
-                    key={chatItem._id}
-                >
-                    {cognitoId == chatItem.user_one.cognito_id ? (
-                        <div className={classes.chillerItemCard}>
-                            <Avatar
-                                alt="userIcon"
-                                src={chatItem.user_two.photo_url}
-                                className={classes.avatar}
-                            />
-                            <p>{chatItem.user_two.name}</p>
-                        </div>
-                    ) : (
-                        <div className={classes.chillerItemCard}>
-                            <Avatar
-                                alt="userIcon"
-                                src={chatItem.user_one.photo_url}
-                                className={classes.avatar}
-                            />
-                            <p>{chatItem.user_one.name}</p>
-                        </div>
-                    )}
-                </Card>
-            ))} */}
             <List>
                 {chatboxes.map((chatItem) => (
                     <ListItem
                         button
                         key={chatItem._id}
-                        onClick={onClickChatItem}
+                        onClick={() => onChatItem(chatItem._id)}
                     >
                         {cognitoId == chatItem.user_one.cognito_id ? (
-                            <div className={classes.chillerItemCard}>
+                            <div className={classes.chillerItemCardDrawer}>
                                 <Avatar
                                     alt="userIcon"
                                     src={chatItem.user_two.photo_url}
-                                    className={classes.avatar}
+                                    className={classes.avatarDrawer}
                                 />
-                                <p>{chatItem.user_two.name}</p>
+                                <h4 className={classes.textDrawer}>
+                                    {chatItem.user_two.name}
+                                </h4>
                             </div>
                         ) : (
-                            <div className={classes.chillerItemCard}>
+                            <div className={classes.chillerItemCardDrawer}>
                                 <Avatar
                                     alt="userIcon"
                                     src={chatItem.user_one.photo_url}
-                                    className={classes.avatar}
+                                    className={classes.avatarDrawer}
                                 />
-                                <p>{chatItem.user_one.name}</p>
+                                <h4 className={classes.textDrawer}>
+                                    {chatItem.user_one.name}
+                                </h4>
                             </div>
                         )}
                     </ListItem>
@@ -180,12 +164,6 @@ export default function ChatBox({
     const onMessage = (data) => {
         submitMessage({ chatId: message._id, text: data.message });
     };
-
-    // const onChatItem = (event) => {
-    //     const id = event.target.id;
-    //     console.log(id);
-    //     onClickChatItem(id);
-    // };
 
     const onChatItem = (data) => {
         onClickChatItem(data);
@@ -230,7 +208,7 @@ export default function ChatBox({
                                         src={chatItem.user_two.photo_url}
                                         className={classes.avatar}
                                     />
-                                    <p>{chatItem.user_two.name}</p>
+                                    <h4>{chatItem.user_two.name}</h4>
                                 </div>
                             ) : (
                                 <div className={classes.chillerItemCard}>
@@ -239,7 +217,7 @@ export default function ChatBox({
                                         src={chatItem.user_one.photo_url}
                                         className={classes.avatar}
                                     />
-                                    <p>{chatItem.user_one.name}</p>
+                                    <h4>{chatItem.user_one.name}</h4>
                                 </div>
                             )}
                         </Card>
