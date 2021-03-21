915206f9bc5d4005fd18905e1b4b3f56c0241400
karenm08 <wmok14@my.bcit.ca>
2021-03-20 22:29:47 -0700

    style chat box message

--- afk-and-chill-front-end/src/components/ChatBox/UserMessage/index.js
+++ afk-and-chill-front-end/src/components/ChatBox/UserMessage/index.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
-import { Avatar, Typography } from '@material-ui/core';
+import { Avatar, Typography, Card } from '@material-ui/core';
 import moment from 'moment';
 
 const useStyles = makeStyles((theme) => ({
-    chatboxToRight: {
+    UserChatbox: {
         overflow: 'auto',
         display: 'flex',
     },
-    chatboxToLeft: {
+    chillerChatbox: {
         overflow: 'auto',
         display: 'flex',
         justifyContent: 'flex-end',
@@ -19,9 +19,15 @@ const useStyles = makeStyles((theme) => ({
         marginRight: 10,
         marginBottom: 5,
     },
-    text: {
-        paddingTop: 5,
+    userText: {
+        padding: '2%',
         margin: 10,
+    },
+    chillerText: {
+        padding: '2%',
+        margin: 10,
+        backgroundColor: '#00aeff',
+        color: 'white',
     },
 }));
 
@@ -32,14 +38,14 @@ export default function UserMessage({ message, className, cognitoId }) {
         <>
             {console.log(moment.now())}
             {message.user.cognito_id !== cognitoId ? (
-                <div className={clsx(classes.chatboxToRight, className)}>
+                <div className={clsx(classes.UserChatbox, className)}>
                     <Avatar
                         className={classes.avatar}
                         alt="userIcon"
                         src={message.user.photo_url}
                     ></Avatar>
-                    <Typography
-                        className={classes.text}
+                    <Card
+                        className={classes.userText}
                         variant="body2"
                         color="textPrimary"
                         component="p"
@@ -48,13 +54,13 @@ export default function UserMessage({ message, className, cognitoId }) {
                             'YYYY-MM-DD HH:mm:ss'
                         )}
                         {message.message}
-                    </Typography>
+                    </Card>
                     <Typography component="p"></Typography>
                 </div>
             ) : (
-                <div className={clsx(classes.chatboxToLeft, className)}>
-                    <Typography
-                        className={classes.text}
+                <div className={clsx(classes.chillerChatbox, className)}>
+                    <Card
+                        className={classes.chillerText}
                         variant="body2"
                         color="textPrimary"
                         component="p"
@@ -63,7 +69,7 @@ export default function UserMessage({ message, className, cognitoId }) {
                         {moment(message.timestamp).format(
                             'YYYY-MM-DD HH:mm:ss'
                         )}
-                    </Typography>
+                    </Card>
                     <Avatar
                         className={classes.avatar}
                         alt="userIcon"
